752069573ef9d30e1b0ce6929f7eb212d014da31
muchubatactics <byooxmckeith@gmail.com>
2023-12-15 13:02:32 +0300

    set up complete

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("we are good to go");
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLGlDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC1wYWdlLW9kaW4vLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJ3ZSBhcmUgZ29vZCB0byBnb1wiKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
